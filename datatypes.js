9ad8c40a2716f4fcb72342c75bad03e257dcd370
Joshua024 <adumchimajoshua@gmail.com>
2023-07-22 19:45:48 +0100

    first commit

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+const str = 'String'
+const condition = true 
+const unknown = undefined
+const empty = null
+const num = 1
+const numFloat = 5.2
+
+console.log(typeof str) ;
+console.log(typeof condition);
+console.log(typeof unknown);
+console.log(typeof empty);
+console.log(typeof num);
+console.log(typeof numFloat);
+
+let a, b, c, d 
+let num1 = 1
+let num2 = 2
+let num3 = 3
+let num4 = 4
+
+const firstName = 'Joshua', lastName = 'Adumchimma', maritalStatus = 'Single', country = 'Nigeria', age = 32
+
+const myAge = 35, yourAge = 40
+console.log('I am '+myAge+ ' years old', '\nYou are '+yourAge+ ' years old');
---
+++ variable.js
@@ -0,0 +1,4 @@
+const str = 'String'
+const condi = true 
+const unknown = undefined
+const empty = null
